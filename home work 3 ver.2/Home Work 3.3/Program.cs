b451694d1fe4ff4fe25522182d670efb2fbb1758
Dmitriy <dimukai@yandex.ru>
2021-12-30 19:53:42 +0400

    нечаянно слил дз в мейн еще 1 ссылка

---
+++ home work 3 ver.2/Home Work 3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace домашнее_задание_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                string greeting = System.Console.ReadLine();
+
+                for (int i = greeting.Length - 1; i >= 0; i--)
+                {
+
+                    System.Console.Write(greeting[i]);
+                    
+
+                }            
+                
+            }
+
+        }
+    }
+}
