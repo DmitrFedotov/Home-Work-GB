b8027d97e4c0b5695158e8872aadba88aa95dbb0
Dmitriy <dimukai@yandex.ru>
2021-12-30 19:46:42 +0400

    сходя из своего иследования , я попробывал сделать фибоначчи так

--- Home Work 4/Home Work 4.4/Program.cs
+++ Home Work 4/Home Work 4.4/Program.cs
@@ -6,7 +6,18 @@ namespace Home_Work_4
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i < 1024; i++)
+            {
+                Console.WriteLine(Fib(i));
+            }
            
+        }
+        static long Fib(int n)
+        {
+            if (n < 3)
+                return 1;
+            else
+                return Fib(n - 2) + Fib(n - 1);
         }
     }
 }
