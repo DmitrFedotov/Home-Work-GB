9139c089796522158a7792f1c0ad11648897f6ec
Dmitriy <dimukai@yandex.ru>
2022-01-30 16:48:53 +0400

    no message

---
+++ HomeWork8/HOME WORK 8/HOME WORK 8/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HOME_WORK_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string save = "text.txt";
+            string fileText = File.ReadAllText(save);
+            Console.WriteLine(fileText);
+
+
+            string greeting = Properties.Settings.Default.Greeting;
+
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+
+                Console.WriteLine("Введите имя пользователя:");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string userName = Properties.Settings.Default.UserName;
+
+            Console.WriteLine($"{greeting}, {userName}!");
+
+
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserArge))
+            {
+
+                Console.WriteLine("Введите возраст:");
+                Properties.Settings.Default.UserArge = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string UserArge = Properties.Settings.Default.UserArge;
+
+            Console.WriteLine($"{UserArge}!");
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserPofession))
+            {
+
+                Console.WriteLine("Введите род деаятельности:");
+                Properties.Settings.Default.UserPofession = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string UserPofession = Properties.Settings.Default.UserPofession;
+
+            Console.WriteLine($"{UserPofession}!");
+        }
+    
+    }
+}
